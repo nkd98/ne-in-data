529b12febf19dbecffad2e34a06492a8e8116355
nkd98 <nirujkumardeka@gmail.com>
2026-01-11 15:21:16 +0530

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
@@ -72,6 +73,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <Analytics />
         <Toaster />
       </body>
     </html>
